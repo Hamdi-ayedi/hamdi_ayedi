1ed32dd331882e3324f095ea41a0a281756d3e14
Hamdi-ayedi <hamdi.ayedi@enetcom.u-sfax.tn>
2023-12-21 12:02:57 +0100

    committed

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+// Modifiez l'URL de la base de données pour utiliser MongoDB local
+export const mongoDBURL = 'mongodb://localhost:27017/hamdi';
+// Assurez-vous que "nom-de-votre-base-de-donnees" correspond au nom de votre base de données locale
+
+export const PORT = 5555;
